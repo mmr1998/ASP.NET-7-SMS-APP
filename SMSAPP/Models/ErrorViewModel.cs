e3aca1346566373df898854a7ea34056d3d9df0d
mmr1998 <mr.mmr1998@gmail.com>
2023-01-16 20:37:17 +0600

    Load Basic MVC Project

---
+++ SMSAPP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SMSAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
